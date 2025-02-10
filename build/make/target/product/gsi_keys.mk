2f6452cb138acb5496621a503a62160808ed2294
TegarXLu <136581622+sandrou555@users.noreply.github.com>
2025-02-10 09:21:02 +0700

    Update gsi_keys.mk

--- build/make/target/product/gsi_keys.mk
+++ build/make/target/product/gsi_keys.mk
@@ -1,1 +1,32 @@
-1
+# gsi_keys.mk TWRP Manifest 14
+
+# Inherit common GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Including GSI Keys for authentication
+PRODUCT_PACKAGES += \
+    qemu-gsi.avbpubkey
+
+# Path to GSI key
+GSI_KEY_PATH := external/avb/test/data
+
+# Add a special AVB key
+PRODUCT_AVB_PUBLIC_KEY := $(GSI_KEY_PATH)/testkey_rsa2048.pem
+PRODUCT_AVB_ALGORITHM := SHA256_RSA2048
+
+# Enable GSI-specific flags
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.gsi.image=true \
+    ro.treble.enabled=true
+
+# Copy GSI keys ke partisi system
+PRODUCT_COPY_FILES += \
+    $(GSI_KEY_PATH)/testkey_rsa2048.pem:$(TARGET_COPY_OUT_SYSTEM)/etc/security/avb/gsi_public_key.pem
+
+# Workaround for dynamic partitions
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.dynamic_partitions=true
+
+#  Dummy Security Patch
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.version.security_patch=$(PLATFORM_SECURITY_PATCH)
